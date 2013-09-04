2d2549566af104c50d0bd41b6e2633d6b039bc37
David Gauld <dcgauld@gmail.com>
2013-09-04 10:17:26 +0100

    Changed name and altered variable names

--- includes/load.php
+++ includes/load.php
@@ -1,120 +1,120 @@
 <?php
 
 /**
- * The YaffCMS class.
+ * The Markdown-CMS class.
  *
  * @version 0.1
  */
-class Yaff {
+class MarkdownCMS {
 
-	/**
-	 * Constructor.
-	 *
-	 * Gathers URL and retrieves .md file accordingly. Parses through to the
-	 * Markdown class and includes the relevant page template.
-	 *
-	 * @since 0.1
-	 */
-	public function __construct() {
-		global $y;
-		if (isset($y['base_url']) && $y['base_url']) {
-			$this->apply_default_settings();
+    /**
+     * Constructor.
+     *
+     * Gathers URL and retrieves .md file accordingly. Passes through to the
+     * Markdown class and includes the relevant page template.
+     *
+     * @since 0.1
+     */
+    public function __construct() {
+        global $config;
+        if (isset($config['base_url']) && $config['base_url']) {
+            $this->apply_default_settings();
 
-			if ($_SERVER['REQUEST_URI'] != $_SERVER['PHP_SELF'])
-				$url = trim(preg_replace('/' . str_replace('/', '\/', str_replace('index.php', '', $_SERVER['PHP_SELF'])) . '/', '', $_SERVER['REQUEST_URI'], 1), '/');
+            if ($_SERVER['REQUEST_URI'] != $_SERVER['PHP_SELF'])
+                $url = trim(preg_replace('/' . str_replace('/', '\/', str_replace('index.php', '', $_SERVER['PHP_SELF'])) . '/', '', $_SERVER['REQUEST_URI'], 1), '/');
 
-			$file = ($url ? CONTENT_DIR . $url : CONTENT_DIR . 'index');
+            $file = ($url ? CONTENT_DIR . $url : CONTENT_DIR . 'index');
 
-			is_dir($file) ? $file = CONTENT_DIR . $url . '/index.md' : $file .= '.md';
+            is_dir($file) ? $file = CONTENT_DIR . $url . '/index.md' : $file .= '.md';
 
-			if (file_exists($file)) {
-				$y['page_content'] = file_get_contents($file);
-			} else {
-				$y['page_content'] = file_get_contents(CONTENT_DIR . '404.md');
-				header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-			}
+            if (file_exists($file)) {
+                $config['page_content'] = file_get_contents($file);
+            } else {
+                $config['page_content'] = file_get_contents(CONTENT_DIR . '404.md');
+                header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+            }
 
-			$this->get_page_meta();
-			$y['page_content'] = preg_replace('/<!--[\s\S]*?-->/', '', $y['page_content']);
-			$this->extract_vars();
-			$y['page_content'] = Markdown($y['page_content']);
+            $this->get_page_meta();
+            $config['page_content'] = preg_replace('/<!--[\s\S]*?-->/', '', $config['page_content']);
+            $this->extract_vars();
+            $config['page_content'] = Markdown($config['page_content']);
 
-			include(THEMES_DIR . $y['theme'] . '/' . $y['page_template'] . '.php');
-		} else {
-			die('\'base_url\' value does not exist. Please add the desired URL of your installation in \'settings.php\'');
-		}
-		$this->get_posts();
-	}
-	
-	/**
-	 * Retrieves the meta information from a .md page file and adds the fields
-	 * to the global $y array.
-	 *
-	 * @since 0.1
-	 */
-	public function get_page_meta() {
-		global $y;
-		$meta = array(
-			'page_title'	=> 'Page Title',
-			'page_template'	=> 'Page Template'
-		);
-		$defaults = array(
-			'page_title'	=> $y['site_title'],
-			'page_template'	=> 'default'
-		);
-	 	foreach ($meta as $field => $value) {
-			if (preg_match('/^[ \t\/*#@]*' . preg_quote($value, '/') . ':(.*)$/mi', $y['page_content'], $match) && $match[1]) {
-				$y[$field] = trim(preg_replace('/\s*(?:\*\/|\?>).*/', '', $match[1]));
-			} else {
-				$y[$field] = $defaults[$field];
-			}
-		}
-	}
+            include(THEMES_DIR . $config['theme'] . '/' . $config['page_template'] . '.php');
+        } else {
+            die('\'base_url\' value does not exist. Please add the desired URL of your installation in \'settings.php\'');
+        }
+        $this->get_posts();
+    }
+    
+    /**
+     * Retrieves the meta information from a .md page file and adds the fields
+     * to the global $config array.
+     *
+     * @since 0.1
+     */
+    public function get_page_meta() {
+        global $config;
+        $meta = array(
+            'page_title'	=> 'Page Title',
+            'page_template'	=> 'Page Template'
+        );
+        $defaults = array(
+            'page_title'	=> $config['site_title'],
+            'page_template'	=> 'default'
+        );
+         foreach ($meta as $field => $value) {
+            if (preg_match('/^[ \t\/*#@]*' . preg_quote($value, '/') . ':(.*)$/mi', $config['page_content'], $match) && $match[1]) {
+                $config[$field] = trim(preg_replace('/\s*(?:\*\/|\?>).*/', '', $match[1]));
+            } else {
+                $config[$field] = $defaults[$field];
+            }
+        }
+    }
 
-	/**
-	 * Extracts variables from a .md page file. If the user types %hello%,
-	 * this will look in $y[hello] to find the required value (settings.php).
-	 *
-	 * @since 0.1
-	 */
-	public function extract_vars() {
-		global $y;
-		foreach ($y as $field => $value) {
-			if ($field != 'page_content')
-				$y['page_content'] = str_replace('%' . $field . '%', $value, $y['page_content']);
-		}
-	}
+    /**
+     * Extracts variables from a .md page file. If the user types %hello%,
+     * this will look in $config[hello] to find the required value (settings.php).
+     *
+     * @since 0.1
+     */
+    public function extract_vars() {
+        global $config;
+        foreach ($config as $field => $value) {
+            if ($field != 'page_content')
+                $config['page_content'] = str_replace('%' . $field . '%', $value, $config['page_content']);
+        }
+    }
 
-	/**
-	 * Ensures that the 3 required options (site_title, base_url and theme) are
-	 * never left blank by adding default values if this is the case.
-	 *
-	 * @since 0.1
-	 */
-	public function apply_default_settings() {
-		global $y;
-		$defaults = array(
-			'site_title'	=> 'YaffCMS',
-			'theme'			=> 'default',
-		);
-		foreach ($defaults as $field => $value) {
-			if (!array_key_exists($field, $y)) {
-				$y[$field] = $value;
-			}
-		}
-		$y['theme_url'] = $y['base_url'] . 'themes/' . $y['theme'];
-	}
+    /**
+     * Ensures that the 3 required options (site_title, base_url and theme) are
+     * never left blank by adding default values if this is the case.
+     *
+     * @since 0.1
+     */
+    public function apply_default_settings() {
+        global $config;
+        $defaults = array(
+            'site_title'	=> 'Markdown-CMS',
+            'theme'			=> 'default',
+        );
+        foreach ($defaults as $field => $value) {
+            if (!array_key_exists($field, $config)) {
+                $config[$field] = $value;
+            }
+        }
+        $config['theme_url'] = $config['base_url'] . 'themes/' . $config['theme'];
+    }
 
-	/**
-	 * Adds the ability to loop through 'posts'. Needs to be completed.
-	 *
-	 * @since 0.1.1
-	 */
-	public function get_posts() {
-		global $y;
-		$y['posts'] = glob(POSTS_DIR . "*.md");
-	}
-	
+    /**
+     * Adds the ability to loop through 'posts'. Needs to be completed.
+     *
+     * @since 0.1
+     */
+    public function get_posts() {
+        global $config;
+        $config['posts'] = glob(POSTS_DIR . "*.md");
+    }
+    
 }
 
 ?>
