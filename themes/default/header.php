ae4522a580e52a833b1dfc285af232aabd787172
David Gauld <dcgauld@gmail.com>
2013-09-04 10:21:33 +0100

    Updated yet another variable name

--- themes/default/header.php
+++ themes/default/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo $y['page_title']; ?> | <?php echo $y['site_title'] ?></title>
-	<link rel="stylesheet" href="<?php echo $y['theme_url']; ?>/style.css" type="text/css" />
+	<title><?php echo $config['page_title']; ?> | <?php echo $config['site_title'] ?></title>
+	<link rel="stylesheet" href="<?php echo $config['theme_url']; ?>/style.css" />
 </head>
 <body>
