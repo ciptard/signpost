e2322bb84a21d90db4a58e29724f032b7d98cc71
David Gauld <dcgauld@gmail.com>
2013-09-17 18:15:16 +0100

    Fixed variable naming error

--- settings.php
+++ settings.php
@@ -1,8 +1,8 @@
 <?php
 
-$y = array(
+$config = array(
 	// Change the line below before you do anything else.
-	'base_url' => 'http://localhost/',
+	'base_url' => 'http://localhost:8888/projects/markdown-cms/',
 
 	/*
 	 * Uncomment the lines below to override the default settings.
