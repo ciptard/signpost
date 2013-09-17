413c3ae88a033c7ed49004de84893f7315889279
David Gauld <dcgauld@gmail.com>
2013-09-17 18:15:25 +0100

    Added test code for get_pages() function

--- themes/default/default.php
+++ themes/default/default.php
@@ -2,6 +2,16 @@
 
 <div id="container">
 	<?php echo $config['page_content']; ?>
+  <?php $posts = $this->get_pages();
+echo "hi";
+foreach ($posts as $post) {
+  echo '<h2><a href="' . $post->url . '">' . $post->title . '</a></h2>';
+  echo '<p>' . $post->content . '</p>';
+}
+
+?>
 </div>
+
+
 
 <?php include('footer.php'); ?>
