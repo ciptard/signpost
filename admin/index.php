336b695ae417fa42a3ae860600bc0237e3c113dc
David Gauld <dcgauld@gmail.com>
2013-09-18 13:01:18 +0100

    Added links to admin JavaScript and CSS files

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title>Markdown-CMS Admin</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="js/showdown.js"></script>
+    <script src="js/main.js"></script>
   </head>
   <body>
   </body>
