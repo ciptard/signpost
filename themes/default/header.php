e431bf6ed24ca0e667df9acdd43d24ae821c9753
David Gauld <dcgauld@gmail.com>
2013-09-19 12:35:37 +0100

    Updated default theme's title to reflect new variable

--- themes/default/header.php
+++ themes/default/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo $config['page_title']; ?> | <?php echo $config['site_title'] ?></title>
+	<title><?php echo $page->title; ?> | <?php echo $config['site_title'] ?></title>
 	<link rel="stylesheet" href="<?php echo $config['theme_url']; ?>/style.css" />
 </head>
 <body>
