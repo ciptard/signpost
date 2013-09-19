516b34d6aeb3f5f0c915a1747b1ceb4515d49980
David Gauld <dcgauld@gmail.com>
2013-09-19 12:30:42 +0100

    Cleaned up blog template

---
+++ themes/default/blog.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+
+<div id="container">
+  <?php foreach ($posts as $post) {
+    echo '<h2><a href="' . $post->url . '">' . $post->title . '</a></h2>';
+    echo '<p>' . $post->content . '</p>';
+  } ?>
+</div>
+
+<?php include('footer.php'); ?>
