77ad3b58764ab037f1ded9eee0278142f513d811
David Gauld <dcgauld@gmail.com>
2013-09-19 08:31:13 +0100

    Altered demo implementation

--- themes/default/default.php
+++ themes/default/default.php
@@ -1,10 +1,8 @@
 <?php include('header.php'); ?>
 
 <div id="container">
-	<?php echo $config['page_content']; ?>
-  <?php $posts = $this->get_pages();
-echo "hi";
-foreach ($posts as $post) {
+	<?php echo $page->content; ?>
+  <?php foreach ($posts as $post) {
   echo '<h2><a href="' . $post->url . '">' . $post->title . '</a></h2>';
   echo '<p>' . $post->content . '</p>';
 }
