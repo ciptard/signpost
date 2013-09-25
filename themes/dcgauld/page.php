d22b5ff73e54343678c19e8894b512b1671660b8
David Gauld <dcgauld@gmail.com>
2013-09-25 23:53:17 +0100

    Added <article> tags to ensure pages follow same format as posts

--- themes/dcgauld/page.php
+++ themes/dcgauld/page.php
@@ -3,8 +3,10 @@
 <?php include('profile.php'); ?>
 
 <section class="container">
+<article>
   <small><a href="<?php echo $config['base_url']; ?>">&larr; Back to homepage</a></small>
   <?php echo $page->content; ?>
+</article>
 </section>
 
 <?php include('footer.php'); ?>
