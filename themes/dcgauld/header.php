7e971f81a0424772ae67f32bab423a01dd0aae28
David Gauld <dcgauld@gmail.com>
2013-09-26 00:00:43 +0100

    Adjusted title to only show 'site_title' on homepage

--- themes/dcgauld/header.php
+++ themes/dcgauld/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo $page->title; ?> | <?php echo $config['site_title'] ?></title>
+	<title><?php if (!$page->is_front_page) { ?><?php echo $page->title; ?> | <?php } ?><?php echo $config['site_title'] ?></title>
   <link rel="stylesheet" href="<?php echo $config['theme_url']; ?>/css/normalize.css" />
 	<link rel="stylesheet" href="<?php echo $config['theme_url']; ?>/css/style.css" />
   <script src="//use.typekit.net/yoi6rpo.js"></script>
