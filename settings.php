bde66c558d2982e4a6e4add4af50b1ba83adccfc
David Gauld <dcgauld@gmail.com>
2013-09-28 18:25:35 +0100

    Removed instructions (to be added to README)

--- settings.php
+++ settings.php
@@ -10,14 +10,6 @@ $config = array(
 
 	// 'site_title' => 'Markdown-CMS',
 	'theme' => 'dcgauld'
-
-	/*
-	 * You can also create your own custom setting and use them in your .php and
-	 * .md files. For example, a setting called 'hello' can be accessed by typing
-	 * %hello% in your page's .md file or $config['hello'] in your .php files.
-	 */
-
-	// 'hello' => 'custom_value'
 );
 
 ?>
